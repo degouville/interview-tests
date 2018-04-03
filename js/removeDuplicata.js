bde01060eb928f3e8c6f388ef192d28d5e811c1f
Mathieu <mathieu@degouville.com>
2018-04-03 17:05:18 +0200

    Add the "remove duplicates values in an Array" question

---
+++ js/removeDuplicata.js
@@ -0,0 +1,16 @@
+// QUESTION: Let say you have an Array with duplicate values,
+// ...are would you return this Array without duplicates ?
+
+// NOTE: let's make a randmon Array with duplicates
+const myArrayWithDuplicates = [2, 3, 3, 10, 2, 5, 1]
+
+// 1st: I'll use Set and create a set from the given array
+// 2nd: Given that Set return an Object, I'll spread it into a new Array
+// BONUS: now I just want to get this array sorted so I use the .sort function
+// ... but 10 came before 2 so I fix sort with (a, b) => a - b as callback
+const removeDuplicate = arr => [...new Set(arr)].sort((a, b) => a - b)
+
+// NOTE: Test the removeDuplicate() function
+const myArrayWithoutDuplicates = removeDuplicate(myArrayWithDuplicates)
+
+console.log(myArrayWithoutDuplicates)
